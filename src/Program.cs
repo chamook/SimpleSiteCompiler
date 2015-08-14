41878d426b974a92481af8869bf017edd949218a
Gareth Lowe <gareth.lowe@ao.com>
2015-08-14 09:56:26 +0100

    Hello world console app

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SimpleSiteCompiler
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
